b0ab8a8af960ef890bc8ac6adbe5b61cb8079de2
MalcolmDahling <mac.dahling@gmail.com>
2021-11-17 19:34:28 +0100

    Uppgift klar.

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById('spara').addEventListener('click', function(){
+    document.getElementById('textInput').value = document.getElementById('textInput').value.toUpperCase();
+});
